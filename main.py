91e642beda7acbaf8a01639065e5ac21a9c48af7
carstenSukatsch <121088867+carstenSukatsch@users.noreply.github.com>
2022-12-22 15:22:35 +0100

    dev

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-schreib schreib
+# schreib schreib
+# korrektur
 
 from lib.l1 import lib1
 
