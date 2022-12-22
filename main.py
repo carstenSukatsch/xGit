343025aa85597e9dfc8c507dbdde65b82b536d66
carstenSukatsch <121088867+carstenSukatsch@users.noreply.github.com>
2022-12-22 15:18:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+schreib schreib
 
 from lib.l1 import lib1
 
